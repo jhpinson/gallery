1cc39d5c85e00b8fc070a39dd673282efc495e87
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-07-27 15:38:18 +0200

    update django-base

--- settings/development.py
+++ settings/development.py
@@ -12,15 +12,11 @@ PIPELINE_VERSIONING = 'pipeline.versioning.mtime.MTimeVersioning'
 
 PIPELINE_STYLUS_BINARY = check_output(['which','stylus'])
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
 INTERNAL_IPS = ('127.0.0.1')
 
 INSTALLED_APPS += (
     'devserver',
     'resetdb',
-    'debug_toolbar',
 )
 
 DEVSERVER_MODULES = (
