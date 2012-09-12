44469cef0ddb9158d78de108ad6402f247ae3255
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-09-12 21:42:21 +0200

    wsgi

---
+++ wsgi.py
@@ -0,0 +1,12 @@
+import os
+from path import path
+import sys
+sys.path.append(path(__file__).abspath().dirname())
+os.environ["CELERY_LOADER"] = "django"
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production_oldurl'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
+import cacheops
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+applications = {'/':'application', }
