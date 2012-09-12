66e2bbc6be815146acc37a3d3a488fdf29105073
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-09-12 20:24:25 +0200

    production

--- settings/production.py
+++ settings/production.py
@@ -2,27 +2,15 @@ from common import *
 from pipeline import *
 from subprocess import check_output
  
-DEBUG = False
+DEBUG = True
+
+DISABLE_ROBOTS=True
+
 PIPELINE_AUTO = True
 PIPELINE_VERSION = False
 PIPELINE_VERSIONING = 'pipeline.versioning.mtime.MTimeVersioning'
 
 PIPELINE_STYLUS_BINARY = check_output(['which','stylus'])
 
+INTERNAL_IPS = ('127.0.0.1')
 
-TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    )),
-)
-
-INSTALLED_APPS += (
-    'raven.contrib.django',
-)
-
-if SENTRY_SITE is None:
-    raise Exception('You must define common.SENTRY_SITE')
-
-if SENTRY_DSN is None:
-    raise Exception('You must define common.SENTRY_DSN')
