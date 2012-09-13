271c7fe3d2df4935a3b5219ce14c9b5c40b6dc50
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-09-13 08:30:43 +0200

    cacheops

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,6 @@ sys.path.append(path(__file__).abspath().dirname())
 os.environ["CELERY_LOADER"] = "django"
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production_oldurl'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.production'
-import cacheops
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
 
