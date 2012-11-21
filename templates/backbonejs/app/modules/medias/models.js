125132089a39db81bd64de6308e84dab9f934915
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-11-21 14:14:33 +0100

    for facetting facility add attributes year, month day on models Media

--- templates/backbonejs/app/modules/medias/models.js
+++ templates/backbonejs/app/modules/medias/models.js
@@ -15,7 +15,12 @@ function(app) {
     urlRoot: '/rest/medias',
 
     // Ensure that each todo created has `title`.
-    initialize: function() {},
+    initialize: function() {
+      var date = (/([0-9]{4})-([0-9]{2})-([0-9]{2})/).exec(this.get('date'));
+      this.set('year', date[1]);
+      this.set('month', date[2]);
+      this.set('day', date[3]);
+    },
 
     get_url : function () {
       if (this.get('is_an_album') == 1) {
